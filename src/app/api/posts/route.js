be4fb1f6614f5d2415a78c2935aadc96daf19591
Prakash <itsipnp@gmail.com>
2023-06-26 13:53:20 +0545

    removed a file

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,28 +1,27 @@
+// import { NextResponse } from "next/server";
+// import connect from "@/utils/db";
+// import Post from "@/models/Post";
+
+// export const GET = async (request) => {
+//   try {
+//     await connect();
+//     const posts = await Post.find();
+//     return NextResponse.json({ posts });
+//   } catch (err) {
+//     return new NextResponse("Database Error", { status: 500 });
+//   }
+// };
+
 import { NextResponse } from "next/server";
 import connect from "@/utils/db";
 import Post from "@/models/Post";
 
 export const GET = async (request) => {
   try {
-    console.log("Before database connection");
-    await connect();
-    console.log("After database connection");
-    return new NextResponse(JSON.stringify(posts), { status: 200 });
-    // Rest of the code...
-  } catch (err) {
-    console.error("Error:", err);
-    return new NextResponse("err");
-  }
-};
-
-export const GET = async (request) => {
-  try {
-    await connect();
+    await connect(); // Call the connect function before executing the query
     const posts = await Post.find();
-    return new NextResponse(JSON.stringify(posts), { status: 200 });
+    return NextResponse.json({ posts });
   } catch (err) {
-    return new NextResponse("err");
+    return new NextResponse("Database Error", { status: 500 });
   }
 };
-
-
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,10 +1,26 @@
+// import mongoose from "mongoose";
+
+// const connect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO);
+//   } catch (error) {
+//     throw new Error("Connection failed!");
+//   }
+// };
+
+// export default connect;
+
 import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO);
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to database");
   } catch (error) {
-    throw new Error("Connection failed!");
+    console.error("Connection failed:", error);
   }
 };
 
